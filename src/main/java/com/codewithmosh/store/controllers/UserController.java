13b3daa8004605e8f714902ebd68aa46d1d463eb
melih <barisik.melih@icloud.com>
2025-07-03 15:34:56 +0300

    creating controller and using dto, mapper

---
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -0,0 +1,40 @@
+package com.codewithmosh.store.controllers;
+
+import com.codewithmosh.store.dtos.UserDto;
+import com.codewithmosh.store.mappers.UserMapper;
+import com.codewithmosh.store.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RequestMapping("/users")
+@RestController
+public class UserController {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @GetMapping()
+    public List<UserDto> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(userMapper::toDto)
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            //  return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return ResponseEntity.notFound().build();
+        }
+        // return new ResponseEntity<>(user, HttpStatus.OK);
+        return ResponseEntity.ok(userMapper.toDto(user));
+    }
+}
