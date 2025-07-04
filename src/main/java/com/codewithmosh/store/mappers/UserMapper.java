408531327d979525f75a4b64bb9cc5a3adc8f7e4
melih <barisik.melih@icloud.com>
2025-07-04 15:38:30 +0300

    user create api

--- src/main/java/com/codewithmosh/store/mappers/UserMapper.java
+++ src/main/java/com/codewithmosh/store/mappers/UserMapper.java
@@ -1,4 +1,5 @@
 package com.codewithmosh.store.mappers;
+import com.codewithmosh.store.dtos.RegisterUserRequest;
 import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
 import org.mapstruct.Mapper;
@@ -6,4 +7,5 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDto toDto(User user);
+    User toEntity(RegisterUserRequest user);
 }
