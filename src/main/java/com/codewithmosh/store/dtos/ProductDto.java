5df1905b2f440aaef9b1b7312c7acf0a523d8bd5
melih <barisik.melih@icloud.com>
2025-07-05 20:51:06 +0300

    product create, update and delete apis

--- src/main/java/com/codewithmosh/store/dtos/ProductDto.java
+++ src/main/java/com/codewithmosh/store/dtos/ProductDto.java
@@ -3,11 +3,13 @@ package com.codewithmosh.store.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @AllArgsConstructor
 @Getter
+@Setter
 public class ProductDto {
     private Long id;
     private String name;
