2c3b0fb6ec575c85fc766911a39116a8dc000802
melih <barisik.melih@icloud.com>
2025-08-03 08:48:05 +0300

    building a webhook endpoint

--- src/main/java/com/codewithmosh/store/config/SecurityConfig.java
+++ src/main/java/com/codewithmosh/store/config/SecurityConfig.java
@@ -58,6 +58,8 @@ public class SecurityConfig {
                      .requestMatchers("/admin/**").hasRole(Role.ADMIN.name())
                      .requestMatchers(HttpMethod.POST, "/users").permitAll()
                      .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                     .requestMatchers(HttpMethod.POST, "/auth/refresh").permitAll()
+                     .requestMatchers(HttpMethod.POST, "/checkout/webhook").permitAll()
                      .anyRequest().authenticated()
              ).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(c-> {
